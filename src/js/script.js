2c8c093782a3b37fcb4dd2c29d59ebaae33170ae
Mary <ya.kashina-marina@yandex.ru>
2020-10-30 19:55:11 +0500

    carousel commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,21 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
-
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+$(document).ready(function(){
+    $('.carousel_inner').slick({
+        infinite: true,
+        speed: 1200,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/prevArrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/nextArrow.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }  
+            }
+        ]
+      });
+  });
